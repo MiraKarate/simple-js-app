9553708d07e7aa3519caf02d5eac80337bd77ada
MiraKarate <mira.herrarte@gmail.com>
2023-02-17 11:14:10 +0100

    created basic pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood="Avocado"
-document.write(favoriteFood)
+let pokemonList = [
+    { name: 'Bulbasur', height: 0.7, weight: 6.9, types: ['grass', 'poison']}
+    { name: 'Seadra', height: 1.2, weight: 25, types: ['water']}
+    { name: 'Pikachu', height: 0.4, weight: 6, types: ['electirc']}
+    { name: 'Nidoking', height: 1.4, weight: 62, types: ['ground', 'poison']}
+]
