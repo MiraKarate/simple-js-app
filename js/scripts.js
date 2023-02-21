10f5ef188ecb90140186ab9378dcbcd872efcdd5
MiraKarate <mira.herrarte@gmail.com>
2023-02-21 13:45:39 +0100

    changed background and text color in body

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,10 @@ let pokemonList = [
     ];
 
 for(let i = 0; i < pokemonList.length; i++){ 
+    if (pokemonList[i].height > 1.3){
+        document.write(" " + pokemonList[i].name + " (height:" + pokemonList[i].height + ") -Wow, that’s big!");
+    }else{
     document.write(" " + pokemonList[i].name + " (height:" + pokemonList[i].height + "); ");
+    }
 }
+
