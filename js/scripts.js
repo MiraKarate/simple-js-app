77c1296e7e427a7ad467c26b7a1331d080cadcc8
MiraKarate <mira.herrarte@gmail.com>
2023-02-21 13:12:57 +0100

    add: for loop to show list of Pokemon(height)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 let pokemonList = [
-    { name: 'Bulbasur', height: 0.7, weight: 6.9, types: ['grass', 'poison']}
-    { name: 'Seadra', height: 1.2, weight: 25, types: ['water']}
-    { name: 'Pikachu', height: 0.4, weight: 6, types: ['electirc']}
-    { name: 'Nidoking', height: 1.4, weight: 62, types: ['ground', 'poison']}
-]
+    {name: 'Bulbasur', height: 0.7, weight: 6.9, types: ['grass', 'poison']},
+    {name: 'Seadra', height: 1.2, weight: 25, types: ['water']},
+    {name: 'Pikachu', height: 0.4, weight: 6, types: ['electirc']},
+    {name: 'Nidoking', height: 1.4, weight: 62, types: ['ground', 'poison']}
+    ];
+
+for(let i = 0; i < pokemonList.length; i++){ 
+    document.write(" " + pokemonList[i].name + " (height:" + pokemonList[i].height + "); ");
+}
