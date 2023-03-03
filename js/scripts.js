77d43496fd4009ee438d188177918202b92b6cd5
MiraKarate <mira.herrarte@gmail.com>
2023-03-03 12:13:05 +0100

    created addListItem() in IIFE, added an event listener to each button, updated UI
    Replaced the document.write(...) with functions like createElement() and appendChild(), added classes by using classList functions to create a list of items: <li></li>. Each one of these list items holds a <button></button> element with a Pokémon's name as its inner text.
    
    Updated UI and build an interface showing a list of all your Pokémon, and when you click on one of them, the console logs its name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,72 @@
 
 let pokemonRepository = (function () {
     let pokemonList = [
-        {name: 'Bulbasur', height: 0.7, weight: 6.9, types: ['grass', 'poison']},
-        {name: 'Seadra', height: 1.2, weight: 25, types: ['water']},
-        {name: 'Pikachu', height: 0.4, weight: 6, types: ['electirc']},
-        {name: 'Nidoking', height: 1.4, weight: 62, types: ['ground', 'poison']},
-        ];
+        {name: 'Bulbasur', 
+        height: 0.7, 
+        weight: 6.9, 
+        types: ['grass', 'poison']
+        },
+        {name: 'Seadra', 
+        height: 1.2, 
+        weight: 25, 
+        types: ['water']
+        },
+        {name: 'Pikachu', 
+        height: 0.4, 
+        weight: 6, 
+        types: ['electirc']
+        },
+        {name: 'Nidoking',
+        height: 1.4, 
+        weight: 62, 
+        types: ['ground', 'poison']
+        },
+    ];
 
     function add(pokemon) {
-      pokemonList.push(pokemon);
+        if (typeof pokemon === "object" && "name" in pokemon) {
+            pokemonList.push(pokemon);
+        } else {
+            document.write("A pokemon is required.");
+        }
     }
-  
+
     function getAll() {
       return pokemonList;
+    }
+
+    function filterPokemonByName(name) {
+        let result = getAll().filter((pokemon) => pokemon.name == name);
+        return result[0];   // starting index of 0
+    }
+
+    function showDetails(pokemon) {
+        console.log (pokemon);
+    }
+
+    function addListItem(pokemon) {
+        let pokedexList = document.querySelector('.pokemon-list'); //assign variable the ul element (class pokemon-list)
+        let listItem = document.createElement('li'); // Create an li element 
+        let button = document.createElement('button'); // Create a button element
+        button.innerText = pokemon.name; // set buttons innerText to be the Pokémon's name
+        button.classList.add('pokemon-name-button'); // Adds a class to the button using the classList.add method
+        listItem.appendChild(button); // append the button to the list item as its child
+        pokedexList.appendChild(listItem); // append the list item to the unordered list as its child
+        button.addEventListener('click', function(){
+        showDetails(pokemon);
+        });
     }
   
     return {
       add: add,
-      getAll: getAll
+      getAll: getAll,
+      filterPokemonByName: filterPokemonByName,
+      addListItem: addListItem,
+      showDetails: showDetails
     };
-  })();
+})();
 
 pokemonRepository.getAll().forEach(function(pokemon){
-    document.write(pokemon.name + ' (height:' + pokemon.height + ', weight:' + pokemon.weight + ', types:' + pokemon.types + ')<br>');
+    pokemonRepository.addListItem(pokemon);
 });
-  
+ 
